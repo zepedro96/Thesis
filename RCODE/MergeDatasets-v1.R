5a83f1af99182d39324792618c0c5717ff2ad0b2
utilizador <up201404647@fc.up.pt>
2019-01-24 17:06:17 +0000

    Add GHC

--- RCODE/MergeDatasets-v1.R
+++ RCODE/MergeDatasets-v1.R
@@ -2,30 +2,26 @@
 library(raster)
 library(readxl)
 
-# Extract data from raster image ------
-
-extRst_SD <- extract(rst, birdsgrid, fun=sd)
-colnames(extRst_SD) <- paste(colnames(extRst_SD),"_sd",sep="")
-colnames(extRst_AVG) <- paste(colnames(extRst_AVG),"_avg",sep="")
-extRst <- cbind(birdsgrid@data[,c("ID_PSU","ID_SSU","PSU_SSU_ID")], extRst_AVG, extRst_SD)
-write.csv(extRst, "Spring16_extrctBands.csv",row.names = FALSE)
-
-# Read excel data ------
-
-read_excel("C:/Users/Utilizador/Desktop/TESE/Extracts.xlsx")
-extNDVI <- read_xlsx("C:/Users/Utilizador/Desktop/TESE/Extracts_NDVI.xlsx", sheet = "mean_&_stdev_NDVI", col_names = TRUE)
-TotExt <- merge.data.frame(extRst, extNDVI)
-write.csv(TotExt, "Spring16_extrct.csv",row.names = FALSE)
+# Read data ------
 
 Spring16_extrct_v1 <- read.csv("C:/Users/Utilizador/Documents/spring16bands/Spring16_extrct_v1.csv", stringsAsFactors = FALSE)
 
 Passerine <- read.csv("C:/Users/Utilizador/Desktop/TESE/MasterThesisJoseSilva/MasterThesisJoseSilva/DATA/fielddata/FINAL_DATA/PasserineSpeciesDiversity_v1.csv",
-                      , stringsAsFactors = FALSE)
+                      stringsAsFactors = FALSE)
 
 Flora <- read.csv("C:/Users/Utilizador/Desktop/TESE/MasterThesisJoseSilva/MasterThesisJoseSilva/DATA/fielddata/FINAL_DATA/PlantSpeciesDiversity_Vez_v1.csv",
-                  , stringsAsFactors = FALSE)
+                  stringsAsFactors = FALSE)
 
+GHC <- read.csv("C:/Users/Utilizador/Desktop/TESE/MasterThesisJoseSilva/MasterThesisJoseSilva/DATA/fielddata/FINAL_DATA/GHC_LifeFormsDiversity_BySSUid.csv")
 
 vezDF <- merge(Passerine, Flora, by="PSU_SSU_ID")
 vezDF <- merge(vezDF, Spring16_extrct_v1, by="PSU_SSU_ID") 
 vezDF <- vezDF[,-c(40,41,52,53)]
+
+vezDF_GHC <- merge(vezDF, GHC, by="PSU_SSU_ID")
+
+saveRDS(vezDF,"./DATA/vezDF_merge.rds")
+
+vezDF <- readRDS("./DATA/vezDF_merge.rds")
+
+saveRDS(vezDF_GHC,"./DATA/vezDF_GHC_merge.rds")
