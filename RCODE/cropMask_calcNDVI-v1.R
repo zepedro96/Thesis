7ca1a982e715c39a2411a926008021d67d7ebb6b
utilizador <up201404647@fc.up.pt>
2019-01-30 03:04:28 +0000

    Added save of resampled images

--- RCODE/cropMask_calcNDVI-v1.R
+++ RCODE/cropMask_calcNDVI-v1.R
@@ -3,14 +3,32 @@
 library(raster)
 library(sf)
 
-shapeArea <- read_sf("shape_path.shp")
+shapeArea <- read_sf("D:/Thesis/DATA/Vector/VezBasinCOS12_WGS84_29N.shp")
 
-shapeAreaBuff <- st_buffer(shapeArea, 5000)
+shapeAreaBuff <- st_buffer(shapeArea, 2500)
 
-rst <- stack("file")
-rst <- crop(rst, shapeAreaBuff)
-rst <- mask(rst, shapeAreaBuff)
+fl_10m <- list.files("D:/Thesis/DATA/Raster/SentinelS2A/SENTINEL2A_20161229-113456-457_L2A_T29TNG_D_V1-4",
+                 pattern="_SRE_", full.names = TRUE)[c(3,4,5,9)]
 
-ndvi <- (rst[[7]] - rst[[3]]) / (rst[[7]] + rst[[3]])
+fl_20m <- list.files("D:/Thesis/DATA/Raster/SentinelS2A/SENTINEL2A_20161229-113456-457_L2A_T29TNG_D_V1-4",
+                     pattern="_SRE_", full.names = TRUE)[-c(3,4,5,9)]
 
-writeRaster(ndvi, "filename.tif")
+
+rst10m <- stack(fl_10m)
+rst20m <- stack(fl_20m)
+
+rst10m <- crop(rst10m, shapeAreaBuff)
+rst10m <- mask(rst10m, shapeAreaBuff)
+
+rst20m <- crop(rst20m, shapeAreaBuff)
+rst20m <- mask(rst20m, shapeAreaBuff)
+
+rst20m_resamp <- resample(rst20m, rst10m, method="ngb")
+
+rst<- stack(rst10m, rst20m_resamp)
+
+
+ndvi <- (rst10m[[4]] - rst10m[[3]]) / (rst10m[[4]] + rst10m[[3]])
+
+writeRaster(ndvi, "./OUT/NDVI_test.tif")
+
