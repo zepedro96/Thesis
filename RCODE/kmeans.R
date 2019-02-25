03fe8cb4ccbabe7c661d772aa34f23896766623e
utilizador <up201404647@fc.up.pt>
2019-02-25 21:40:19 +0000

    test of kmeans (unsuperclass + predict)

---
+++ RCODE/kmeans.R
@@ -0,0 +1,32 @@
+library(RStoolbox)
+library(raster)
+library(sf)
+library(ggplot2)
+
+#k means Raster
+
+shapeArea <- read_sf("D:/Thesis/DATA/Vector/VezBasinCOS12_WGS84_29N.shp")
+
+shapeAreaBuff <- st_buffer(shapeArea, 2500)
+
+
+fl_10m <- list.files("D:/Thesis/DATA/Raster/SentinelS2A/SENTINEL2A_20161116-112335-459_L2A_T29TNG_D",
+                     pattern="_SRE_", full.names = TRUE)[c(3,4,5,9)]
+
+rst20m_resamp <- raster("D:/Thesis/OUT/autumn16/resamp20_20161116.tif")
+
+ndvi <- raster("D:/Thesis/OUT/autumn16/NDVI_autumn16_20160801.tif")
+
+rst10m <- stack(fl_10m)
+rst10m <- crop(rst10m, shapeAreaBuff)
+rst10m <- mask(rst10m, shapeAreaBuff)
+
+rst<- stack(rst10m, rst20m_resamp, ndvi)
+
+
+uc <- unsuperClass(rst,nClasses = 10)
+
+map <- predict(uc, rst)
+
+
+
