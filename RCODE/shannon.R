6c16c2f04a579b7206f58755152d8f468a78dbff
utilizador <up201404647@fc.up.pt>
2019-03-05 16:30:08 +0000

    Shannon index using kmeans data

---
+++ RCODE/shannon.R
@@ -0,0 +1,71 @@
+library(raster)
+library(rgdal)
+library(fasterize)
+library(sf)
+library(dplyr)
+
+#shannon
+
+shannon <- function (x, na.rm=TRUE){
+  x <- na.omit(x)
+  tb <- table(x)
+  tbrel <- tb / sum(tb)
+  z <- tbrel*log(tbrel)
+  out <- -1*sum(z)
+  return(out)
+}
+
+
+
+
+#birdsgrid <- readOGR("D:/Dropbox/MasterThesisJoséSilva/DATA/FieldData/Birds/Grids/Grid200mEffectSampSSU_WGS84UTM29N_VEZ_v1.shp")
+birdsgrid <- read_sf("D:/Dropbox/MasterThesisJoséSilva/DATA/FieldData/Birds/Grids/Grid200mEffectSampSSU_WGS84UTM29N_VEZ_v1.shp")
+
+length(unique(birdsgrid$ID_SSU))
+
+
+rst <- stack(c("./DATAtoShare/kmeans_winter15_10c.grd", 
+               "./DATAtoShare/kmeans_winter15_20c.grd"))
+
+names(rst) <- c("wint15_km10c","wint15_km20c")
+
+birdGridRst <- fasterize(birdsgrid,rst[[1]],field = "ID_SSU")
+
+rstDF <- na.omit(values(stack(birdGridRst, rst)))
+
+head(rstDF)
+
+rstDFsummary <- rstDF %>% 
+  as.data.frame() %>% 
+  group_by(layer) %>% 
+  summarise_all(.funs = list(shn=shannon), na.rm=TRUE) 
+  #mutate(ID_SSU = layer)
+
+colnames(rstDFsummary)[1] <- "ID_SSU"
+
+write.csv(rstDFsummary, "winter15_shn.csv",row.names = FALSE)
+
+
+
+gridBirds <- birdsgrid %>% left_join(rstDFsummary, by = "ID_SSU")
+
+plot(gridBirds)
+
+write_sf(gridBirds, "./OUT/shn_wint15.shp")
+
+hist(rstDFsummary$wint15_km10c_shn)
+
+
+
+# 
+# extRst_AVG <- extract(rst[[1:3]], birdsgrid, fun = mean, na.rm=TRUE, method="simple")
+# extRst_STD <- extract(rst[[1:3]], birdsgrid, fun = sd, na.rm=TRUE, method="simple")
+# 
+# colnames(extRst_AVG) <- paste("Wint15_",colnames(extRst_AVG),"_avg",sep="")
+# colnames(extRst_STD) <- paste("Wint15_",colnames(extRst_STD),"_std",sep="")
+# 
+# extRst <- cbind(birdsgrid@data[,c("ID_PSU","ID_SSU","PSU_SSU_ID")], 
+#                 extRst_AVG, extRst_STD)
+# 
+# write.csv(extRst, "winter15_extrctPCA.csv",row.names = FALSE)
+# 
