c36a52e4c731e80f6cf95061039082d320d69006
utilizador <up201404647@fc.up.pt>
2019-03-05 16:29:15 +0000

    small change to collumn names

--- RCODE/extract_PrincComponents-v1.R
+++ RCODE/extract_PrincComponents-v1.R
@@ -1,20 +1,44 @@
 
 library(raster)
 library(rgdal)
+library(fasterize)
+library(sf)
+library(dplyr)
+
+#birdsgrid <- readOGR("D:/Dropbox/MasterThesisJoséSilva/DATA/FieldData/Birds/Grids/Grid200mEffectSampSSU_WGS84UTM29N_VEZ_v1.shp")
+birdsgrid <- read_sf("D:/Dropbox/MasterThesisJoséSilva/DATA/FieldData/Birds/Grids/Grid200mEffectSampSSU_WGS84UTM29N_VEZ_v1.shp")
+
+length(unique(birdsgrid$ID_SSU))
 
 
-birdsgrid <- readOGR("C:/Users/Utilizador/Desktop/TESE/MasterThesisJoseSilva/MasterThesisJoseSilva/DATA/fielddata/Birds/Grids","Grid200mEffectSampSSU_WGS84UTM29N_VEZ_v1")
+rst <- stack("./OUT/PCA_S2a_autumn2016.tif")
 
-rst <- stack("./OUT/PCA_S2a_Winter2015.tif")
+birdGridRst <- fasterize(birdsgrid,rst[[1]],field = "ID_SSU")
 
-extRst_AVG <- extract(rst[[1:3]], birdsgrid, fun = mean, na.rm=TRUE, method="simple")
-extRst_STD <- extract(rst[[1:3]], birdsgrid, fun = sd, na.rm=TRUE, method="simple")
+rstDF <- na.omit(values(stack(birdGridRst, rst[[1:3]])))
 
-colnames(extRst_AVG) <- paste("Wint15_",colnames(extRst_AVG),"_avg",sep="")
-colnames(extRst_STD) <- paste("Wint15_",colnames(extRst_STD),"_std",sep="")
+head(rstDF)
 
-extRst <- cbind(birdsgrid@data[,c("ID_PSU","ID_SSU","PSU_SSU_ID")], 
-                extRst_AVG, extRst_STD)
+rstDFsummary <- rstDF %>% 
+  as.data.frame() %>% 
+  group_by(layer) %>% 
+  summarise_all(.funs = list(avg=mean, std=sd), na.rm=TRUE) 
+  #mutate(ID_SSU = layer)
 
-write.csv(extRst, "Spring16_extrctBands.csv",row.names = FALSE)
+colnames(rstDFsummary)[1] <- "ID_SSU"
+ 
+write.csv(rstDFsummary, "autumn16_extrctPCA.csv",row.names = FALSE)
 
+
+# 
+# extRst_AVG <- extract(rst[[1:3]], birdsgrid, fun = mean, na.rm=TRUE, method="simple")
+# extRst_STD <- extract(rst[[1:3]], birdsgrid, fun = sd, na.rm=TRUE, method="simple")
+# 
+# colnames(extRst_AVG) <- paste("Wint15_",colnames(extRst_AVG),"_avg",sep="")
+# colnames(extRst_STD) <- paste("Wint15_",colnames(extRst_STD),"_std",sep="")
+# 
+# extRst <- cbind(birdsgrid@data[,c("ID_PSU","ID_SSU","PSU_SSU_ID")], 
+#                 extRst_AVG, extRst_STD)
+# 
+# write.csv(extRst, "winter15_extrctPCA.csv",row.names = FALSE)
+# 
