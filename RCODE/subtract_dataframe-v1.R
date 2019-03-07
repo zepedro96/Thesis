d4cba061f2351771d6051c9b0325b2bf3fc38886
utilizador <up201404647@fc.up.pt>
2019-03-07 16:51:30 +0000

    corrections to code

--- RCODE/subtract_dataframe-v1.R
+++ RCODE/subtract_dataframe-v1.R
@@ -4,8 +4,9 @@
 
 burnt_plots <- read.csv("D:/Thesis/DATA/fire_birdsgrid.csv")
 
-tbl_data <- read.csv("D:/Thesis/DATAtoShare/winter15_FNL.csv")
+tbl_data <- read.csv("D:/Thesis/DATAtoShare/TABLES/autumn16_FNL.csv")
 
 dlt_burnt <- tbl_data[ !(tbl_data$ID_SSU %in% burnt_plots$ID_SSU), ]
 
-write.csv(er_burnt, "./OUT/dlt_burnt_winter15.csv")
+write.csv(dlt_burnt, "./DATAtoShare/TABLES/dlt_burnt_autumn16.csv")
+
