9bd597fe976475d992085751b9ee32c66d684437
João F Gonçalves <joaofgo@gmail.com>
2019-07-27 22:44:34 +0100

    Changes EFT diversity/evenness trends calculation

--- RCODE/Calculate_RS_EFA_vars/MakeEFTmap_Landsat-v1.R
+++ RCODE/Calculate_RS_EFA_vars/MakeEFTmap_Landsat-v1.R
@@ -8,42 +8,6 @@ library(ggplot2)
 library(RStoolbox)
 library(microbiome)
 
-getVarNames <- function(x) gsub(".tif","",unlist(lapply(strsplit(fpaths,"_"),function(x) x[length(x)])))
-
-fpaths <- list.files("./DATA/RASTER/Landsat/EVI-32day/metrics/jul-jun", pattern=".tif$", full.names = TRUE)[]
-
-
-vnames <- getVarNames(fpaths)
-
-for(i in 1:length(fpaths)){
-  
-  print(i)
-  tmp <- stack(fpaths[[i]])[[31]] # yr=31 -> 2014
-  if(i==1)
-    rst <- stack(tmp)
-  else
-    rst <- stack(rst, tmp)
-}
-
-names(rst) <- vnames
-
-## ------------------------------------------------------------------------------------------------------- ##
-
-avg <- values(rst[["EFAmed"]])
-std <- values(rst[["EFAamp"]])
-smx <- values(rst[["EFAsemax"]])
-
-cor(cbind(avg,std,smx), method="spearman") %>% round(2)
-
-qts <- seq(0, 1, by = 0.25)
-
-avgQt <- quantile(avg, qts, na.rm = TRUE)
-stdQt <- quantile(std, qts, na.rm = TRUE)
-
-recDF <- data.frame(avgCl = as.character(cut(avg, avgQt, labels = 1:4)),
-                    stdCl = as.character(cut(std, stdQt, labels = 1:4)),
-                    mmax  = as.character(smx))
-
 funConcat <- function(x, na.rm=TRUE){
   if(any(is.na(x))){
     return(NA)
@@ -52,17 +16,78 @@ funConcat <- function(x, na.rm=TRUE){
   }
 }
 
-EFT_combns <- apply(recDF, MARGIN = 1, FUN = funConcat)
+getVarNames <- function(x) gsub(".tif","",unlist(lapply(strsplit(fpaths,"_"),function(x) x[length(x)])))
 
-length(unique(EFT_combns))
 
-EFT_map <- raster(rst[[1]])
-values(EFT_map) <- EFT_combns
 
-plot(EFT_map)
+fpaths <- list.files("./DATA/RASTER/Landsat/EVI-32day/metrics/jul-jun", pattern=".tif$", full.names = TRUE)
 
-writeRaster(EFT_map,"./DATA/RASTER/Landsat/EVI-32day/metrics/jul-jun/LT578comp_EVI_1984_2019_jul-jun_EFTcomnbs.tif", 
-            overwrite = TRUE)
+vnames <- getVarNames(fpaths)
+
+
+# Output folder
+outFolder <- "./DATA/RASTER/Landsat/EVI-32day/metrics/jul-jun/EFT"
+# Use quartiles
+qts <- seq(0, 1, by = 0.25)
+# Output years
+yrs <- 1984:2019
+
+
+## ---------------------------------------------------------------- ##
+
+for(in_yr in 1:length(yrs)){
+  
+  yr <- yrs[in_yr]
+  
+  # Load each EFA in the input folder by year 
+  #
+  pb <- txtProgressBar(min=1,max=length(fpaths),style=3)
+  
+  for(i in 1:length(fpaths)){
+    
+    # Load one EFA variable and select the i-th year
+    #
+    tmp <- stack(fpaths[i])[[in_yr]] # yr=31 -> 2014
+    
+    if(i==1)
+      rst <- stack(tmp)
+    else
+      rst <- stack(rst, tmp)
+    
+    setTxtProgressBar(pb, i)
+  }
+  
+  names(rst) <- vnames
+  
+  ## ---------------------------------------------------------------- ##
+  
+  avg <- values(rst[["EFAmed"]])
+  std <- values(rst[["EFAamp"]])
+  smx <- values(rst[["EFAsemax"]])
+  
+  #cor(cbind(avg,std,smx), method="spearman") %>% round(2)
+  
+  avgQt <- quantile(avg, qts, na.rm = TRUE)
+  stdQt <- quantile(std, qts, na.rm = TRUE)
+  
+  recDF <- data.frame(avgCl = as.character(cut(avg, avgQt, labels = 1:4)),
+                      stdCl = as.character(cut(std, stdQt, labels = 1:4)),
+                      mmax  = as.character(smx))
+  
+  EFT_combns <- apply(recDF, MARGIN = 1, FUN = funConcat)
+  
+  message("\n\n-> Found ",length(unique(EFT_combns))," EFT combinations in year ",yr,"!\n\n")
+  
+  EFT_map <- raster(rst[[1]])
+  values(EFT_map) <- EFT_combns
+  
+  #plot(EFT_map)
+  
+  fnOut <- paste(outFolder,"/LT578comp_EVI_jul-jun_EFT_",yr,"_med_amp_semax_025qts_v1.tif",sep="")
+  writeRaster(EFT_map, fnOut, overwrite = TRUE)
+  
+}
+
 
 
 
--- RCODE/EFA_EFT_trends/CalcEFA_trends-v1.R
+++ RCODE/EFA_EFT_trends/CalcEFA_trends-v1.R
@@ -12,6 +12,9 @@ library(RStoolbox)
 library(psych)
 library(microbiome)
 
+## ----------------------------------------------- ##
+
+chunk2 <- function(x,n) split(x, cut(seq_along(x), n, labels = FALSE)) 
 
 divEvFun <- function(x){
   as.data.frame(
@@ -20,6 +23,24 @@ divEvFun <- function(x){
       eft_count = length(unique(x)))
   )
 }
+
+evCamargo <- function(x) evenness(as.numeric(table(x)), index = "camargo", zeroes = FALSE, detection = 0)[1,,drop=TRUE]
+
+evSimpson <- function(x) evenness(as.numeric(table(x)), index = "simpson", zeroes = FALSE, detection = 0)[1,,drop=TRUE]
+
+evPielou <- function(x) evenness(as.numeric(table(x)), index = "pielou", zeroes = FALSE, detection = 0)[1,,drop=TRUE]
+
+evEvar<- function(x) evenness(as.numeric(table(x)), index = "evar", zeroes = FALSE, detection = 0)[1,,drop=TRUE]
+
+evBulla <- function(x) evenness(as.numeric(table(x)), index = "bulla", zeroes = FALSE, detection = 0)[1,,drop=TRUE]
+
+divShannon <- function(x) diversity(as.numeric(table(x)), index = "shannon", zeroes = FALSE)[1,,drop=TRUE]
+
+divInvSimpson <- function(x) diversity(as.numeric(table(x)), index = "inverse_simpson", zeroes = FALSE)[1,,drop=TRUE]
+
+divGiniSimpson <- function(x) diversity(as.numeric(table(x)), index = "gini_simpson", zeroes = FALSE)[1,,drop=TRUE]
+
+## ----------------------------------------------- ##
 
 shannon <- function (x, na.rm=TRUE){
   x <- na.omit(x)
@@ -43,19 +64,30 @@ countDistinct <- function(x){
   }
 } 
 
-
+## ----------------------------------------------- ##
 
 senSlope <- function(x,...) sens.slope(x,...)$estimates
 senSlopePval <- function(x,...) sens.slope(x,...)$p.value
 
-grid1km   <- read_sf("D:/Dropbox/MasterThesisJoséSilva/DATA/Grids/SampGridVez_1km_Vez_All_v1.shp")
+#grid1km   <- read_sf("D:/Dropbox/MasterThesisJoséSilva/DATA/Grids/SampGridVez_1km_Vez_All_v1.shp")
+grid1km   <- read_sf("D:/MyDocs/Dropbox/MasterThesisJoséSilva/DATA/Grids/SampGridVez_1km_Vez_All_v1.shp")
 
-## Continuous EFA indicators
+## Continuous EFA indicators -----------------------------------------------
 ## EFA_max / EFA_semax
 
-EFA_rst <- stack("./DATA/RASTER/Landsat/EVI-32day/metrics/jul-jun/LT578comp_EVI_1984_2019_jul-jun_EFAavg.tif")
+# EFA's by year
+#
+# EFA_rst <- stack("./DATA/RASTER/Landsat/EVI-32day/metrics/jul-jun/LT578comp_EVI_1984_2019_jul-jun_EFAavg.tif")
 
-EFA_name <- "av" ## USE TWO LETTERS ONLY (shapefiles will not work with more characters... :-0
+## EFT's --------------------------------------------------------------------
+
+# List geotiff files for EFT's by year
+#
+fl <- list.files("./DATA/RASTER/Landsat/EVI-32day/metrics/jul-jun/EFT", pattern=".tif$", full.names = TRUE)
+EFA_rst <- stack(fl)
+
+
+EFA_name <- "et" ## USE TWO LETTERS ONLY (shapefiles will not work with more characters... :-0
 
 names(EFA_rst) <- paste(EFA_name,substr(as.character(1984:2019),3,4),sep="_")
 
@@ -66,14 +98,19 @@ rstStack <- stack(birdGridRst1km, EFA_rst)
 rstDF <- values(rstStack) %>% na.omit
 colnames(rstDF)[1] <- "ID_PSU"
   
-rstDF_agg <- rstDF %>% 
-  as.data.frame %>% 
-  group_by(ID_PSU) %>% 
-  summarize_all(.funs=list("sd"=sd,"av"=mean)) 
+
+## ----------------------------------------------------------------------------------------- ##
+# EFA's
+## ----------------------------------------------------------------------------------------- ##
 
 
+rstDF_agg <- rstDF %>%
+  as.data.frame %>%
+  group_by(ID_PSU) %>%
+  summarize_all(.funs=list("sd"=sd,"av"=mean))
 
-## Estimate trends over different periods -------------------------------------------- ##
+
+## EFA's Estimate trends over different periods -------------------------------------------- ##
 
 # Last ten years trend
 trend_10yr <- rstDF_agg %>% 
@@ -104,5 +141,79 @@ write_sf(grid1km,"./OUTtoSHARE/EFA_trends/EFA_avg_grid1km_1984_2019_v1.shp")
 write_sf(grid1km %>% st_centroid,"./OUTtoSHARE/EFA_trends/EFA_avg_grid1kmCentroids_1984_2019_v1.shp")
 
 
-## Calculate Shannon
+
+## ----------------------------------------------------------------------------------------- ##
+# EFT's
+## ----------------------------------------------------------------------------------------- ##
+
+
+rstDF_agg <- rstDF %>% 
+  as.data.frame %>% 
+  group_by(ID_PSU) %>% 
+  summarize_all(.funs=list(# Evenness indices
+                           "ec"=evCamargo,
+                           "es"=evSimpson,
+                           "ep"=evPielou,
+                           "ee"=evEvar,
+                           "eb"=evBulla,
+                           # Diversity indices
+                           "ds"=divShannon,
+                           "di"=divInvSimpson,
+                           "dg"=divGiniSimpson))
+
+## EFT's Estimate trends over different periods -------------------------------------------- ##
+
+
+#lapply(chunk2(2:289,8), length)
+i <- 0
+chunks <- chunk2(2:ncol(rstDF_agg), 8)
+
+varNames <- c("evCamargo","evSimpson","evPielou","evEvar", "evBulla",
+            "divShannon","divInvSimpson","divGiniSimpson")
+
+for(chunk in chunks){
+  
+  i <- i + 1
+  
+  cat("Doing chunk",i,"out of",length(chunks),"\n\n")
+  
+  print(colnames(rstDF_agg)[chunk[26:35]])
+  cat("\n\n")
+  
+  print(colnames(rstDF_agg)[chunk[16:35]])
+  cat("\n\n")
+  
+  print(colnames(rstDF_agg)[chunk[2:35]])
+  cat("\n\n")
+  
+  # Last ten years trend
+  trend_10yr <- rstDF_agg %>% 
+    select(chunk[26:35]) %>% 
+    data.frame(sen_10yr = apply(.,1,senSlope),pv_10yr = apply(.,1,senSlopePval)) %>% 
+    select(sen_10yr, pv_10yr)
+  
+  trend_20yr <- rstDF_agg %>% 
+    select(chunk[16:35]) %>% 
+    data.frame(sen_20yr = apply(.,1,senSlope),pv_20yr = apply(.,1,senSlopePval)) %>% 
+    select(sen_20yr, pv_20yr)
+  
+  trend_all <- rstDF_agg %>% 
+    select(chunk[2:35]) %>% 
+    data.frame(sen_all = apply(.,1,senSlope),pv_all = apply(.,1,senSlopePval)) %>% 
+    select(sen_all, pv_all)
+  
+  trendsDF <- data.frame(ID_PSU=rstDF_agg$ID_PSU, trend_10yr, trend_20yr, trend_all)
+  
+  grid1km_append <- grid1km %>% 
+    left_join(rstDF_agg[, c(1,chunk)], by = "ID_PSU")%>% 
+    left_join(trendsDF, by = "ID_PSU")
+  
+  outFn1 <- paste("./OUTtoSHARE/EFT_trends/EFT_",varNames[i],"_grid1km_1984_2019_v1.shp",sep="")
+  outFn2 <- paste("./OUTtoSHARE/EFT_trends/EFT_",varNames[i],"_grid1kmCentrs_1984_2019_v1.shp",sep="")
+  
+  write_sf(grid1km_append, outFn1)
+  write_sf(grid1km_append %>% st_centroid, outFn2)
+
+}
+
 
